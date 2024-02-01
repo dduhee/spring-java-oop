1ffd4c8afebd8c3b3bdadf4e3c80d43c8b820bfe
dduhee <jhkyoung0401@naver.com>
2024-02-01 20:57:05 +0900

    Update #3 - Main
    변수 클래스를 인터페이스로 변경
    #3은 여기서 마무리
    스프링으로 넘어감

--- src/main/java/com/dduhee/javaoop/Main.java
+++ src/main/java/com/dduhee/javaoop/Main.java
@@ -1,14 +1,23 @@
 package com.dduhee.javaoop;
 
 import com.dduhee.javaoop.logic.BubbleSort;
+import com.dduhee.javaoop.logic.JavaSort;
+import com.dduhee.javaoop.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> bubbleSort = new BubbleSort<>();
 
-        System.out.println("bubleSort>> "+bubbleSort.sort(Arrays.asList(args)));
+//        new BubbleSort<> 또는 JavaSort<> 로 만들어 사용 >> 강결합
+//        JavaSort<String> bubbleSort = new JavaSort<>( );
+//        System.out.println("구현체> "+bubbleSort.getClass().getName());
+//        System.out.println("bubleSort>> "+bubbleSort.sort(Arrays.asList(args)));
+
+//        이런 강결합을 약하게 하기 위해 인터페이스를 생성하고 각 구현체가 사용하도록 함
+        Sort<String> sort = new JavaSort<>();
+        System.out.println("구현체> "+sort.getClass().getName());
+        System.out.println("sort>> "+sort.sort(Arrays.asList(args)));
 
     }
 }
